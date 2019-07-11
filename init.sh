3e2546043a1fb9dbd40ab638a38d05c7e44b2d0e
syy19940213 <137688788@qq.com>
2019-07-11 14:57:07 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,4 +8,6 @@ mkdir -p /data/docker/nginx/conf/conf.d
 cd /data/docker/nginx/conf
 wget https://github.com/syy19940213/nginx/raw/master/nginx.conf
 
+ip=$(curl http://ipinfo.io/ip)
+curl http://168.63.143.99:8999/server/initSuccessp=$ip
 echo "init success"
