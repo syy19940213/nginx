699b165070ea7ed0ab45c220535fc03c09815114
佘远怡 <sheyy@133.cn>
2019-07-12 17:04:01 +0800

    ssl申请脚本 修改

--- ssl.sh
+++ ssl.sh
@@ -21,3 +21,24 @@ unzip master.zip
 cd certbot-master/
 ./certbot-auto certonly --standalone --email 137688788@qq.com  -d $DOMAIN --non-interactive --agree-tos
 
+mkdir -p /data/docker/nginx/ssl/$DOMAIN
+cp /etc/letsencrypt/archive/$DOMAIN/fullchain1.pem /data/docker/nginx/ssl/$DOMAIN
+cp /etc/letsencrypt/archive/$DOMAIN/privkey1.pem /data/docker/nginx/ssl/$DOMAIN
+cd /data/docker/nginx/ssl/$DOMAIN
+mv privkey1.pem privkey.pem
+mv fullchain1.pem fullchain.pem
+chmod 777 privkey.pem
+chmod 777 fullchain.pem
+
+
+ip=$(curl http://ipinfo.io/ip)
+log=`tail -n 10 /var/log/letsencrypt/letsencrypt.log | grep 'Your certificate and chain have been saved'`
+if [ "$log" == "" ] ; then
+	curl http://168.63.143.99:8999/server/initSsl?ip=$ip&status=2
+	echo "init error"
+else
+	curl http://168.63.143.99:8999/server/initSsl?ip=$ip&status=1
+	echo "init success"
+fi
+
+
