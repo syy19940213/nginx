5c29854326d46ba378f420a47f68653144356627
syy19940213 <137688788@qq.com>
2019-07-13 15:50:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-touch /tmp/init.pid
-yum install -y docker
-sleep 30
-service docker restart
-sleep 30
-docker pull nginx
-mkdir -p /data/docker/nginx/logs
-mkdir -p /data/docker/nginx/ssl
-mkdir -p /data/docker/nginx/conf/conf.d
-cd /data/docker/nginx/conf
-wget https://github.com/syy19940213/nginx/raw/master/nginx.conf
+touch /tmp/init.pid &&
+yum install -y docker &&
+sleep 5 &&
+service docker restart &&
+sleep 10 &&
+docker pull nginx &&
+mkdir -p /data/docker/nginx/logs &&
+mkdir -p /data/docker/nginx/ssl &&
+mkdir -p /data/docker/nginx/conf/conf.d &&
+cd /data/docker/nginx/conf &&
+wget https://github.com/syy19940213/nginx/raw/master/nginx.conf &&
 
-ip=$(curl http://ipinfo.io/ip)
-curl http://http://118.31.108.209:8999/server/initSuccess?ip=$ip
+ip=$(curl http://ipinfo.io/ip) &&
+curl http://http://118.31.108.209:8999/server/initSuccess?ip=$ip &&
 
-rm -rf /tmp/init.pid
-echo "init success"
+rm -rf /tmp/init.pid &&
+echo "init success" &&
