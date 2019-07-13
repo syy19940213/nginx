2a55b70cfa5b81100497aeba78361f1265982428
syy19940213 <137688788@qq.com>
2019-07-13 12:21:08 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+touch /tmp/init.pid
 yum install -y docker
 service docker start
 docker pull nginx
@@ -10,4 +12,6 @@ wget https://github.com/syy19940213/nginx/raw/master/nginx.conf
 
 ip=$(curl http://ipinfo.io/ip)
 curl http://168.63.143.99:8999/server/initSuccess?ip=$ip
+
+rm -rf /tmp/init.pid
 echo "init success"
