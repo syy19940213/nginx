24145fdd20d7f7645f652e8e73b4575e86a704d2
syy19940213 <137688788@qq.com>
2019-07-19 22:18:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -22,7 +22,7 @@ cd /data/docker/nginx/conf &&
 wget https://github.com/syy19940213/nginx/raw/master/nginx.conf &&
 
 cd /data/docker/nginx &&
-docker run -d -p 80:80 -p 443:443 -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/var/log/nginx -v $PWD/conf/conf.d:/etc/nginx/conf.d -v $PWD/ssl:/ssl   --name mynginx --net mynetwork --ip $ip nginx:latest &&
+docker run -d -p 80:80 -p 443:443 -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/var/log/nginx -v $PWD/conf/conf.d:/etc/nginx/conf.d -v $PWD/ssl:/ssl  -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone:ro   --name mynginx  nginx:latest &&
 docker stop mynginx &&
 
 ip=$(curl http://ipinfo.io/ip) &&
