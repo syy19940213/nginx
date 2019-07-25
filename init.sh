0cb5f27392f7f9e8952aa439e306e97d8b30704b
syy19940213 <137688788@qq.com>
2019-07-25 16:01:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,33 +4,33 @@ if [ -f /tmp/init.pid ]; then
     echo 'is running'
     exit 1
 fi
-touch /tmp/init.pid &&
+touch /tmp/init.pid 
 
-ip=$(curl http://ipinfo.io/ip) &&
+ip=$(curl http://ipinfo.io/ip) 
 
-networkip=`echo ${ip%.*}.0` &&
+networkip=`echo ${ip%.*}.0` 
 
 ddd=`yum install -y docker` 
 echo $ddd
 
 sleep 5
 
-service docker restart &&
-docker pull nginx &&
-docker network create --subnet=$networkip/16 mynetwork &&
+service docker restart 
+docker pull nginx 
+docker network create --subnet=$networkip/16 mynetwork 
 
-mkdir -p /data/docker/nginx/logs &&
-mkdir -p /data/docker/nginx/ssl &&
-mkdir -p /data/docker/nginx/conf/conf.d &&
-cd /data/docker/nginx/conf &&
-wget https://github.com/syy19940213/nginx/raw/master/nginx.conf &&
+mkdir -p /data/docker/nginx/logs 
+mkdir -p /data/docker/nginx/ssl 
+mkdir -p /data/docker/nginx/conf/conf.d 
+cd /data/docker/nginx/conf 
+wget https://github.com/syy19940213/nginx/raw/master/nginx.conf 
 
-cd /data/docker/nginx &&
-docker run -d -p 80:80 -p 443:443 -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/var/log/nginx -v $PWD/conf/conf.d:/etc/nginx/conf.d -v $PWD/ssl:/ssl  -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone:ro   --name mynginx  nginx:latest &&
-docker stop mynginx &&
+cd /data/docker/nginx 
+docker run -d -p 80:80 -p 443:443 -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/var/log/nginx -v $PWD/conf/conf.d:/etc/nginx/conf.d -v $PWD/ssl:/ssl  -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone:ro   --name mynginx  nginx:latest 
+docker stop mynginx 
 
-ip=$(curl http://ipinfo.io/ip) &&
-curl http://http://118.31.108.209:8999/server/initSuccess?ip=$ip &&
+ip=$(curl http://ipinfo.io/ip) 
+curl http://http://118.31.108.209:8999/server/initSuccess?ip=$ip 
 
-rm -rf /tmp/init.pid &&
+rm -rf /tmp/init.pid 
 echo "init success"
