8541486098df1d51857e806c4b6e3a678df4d053
syy19940213 <137688788@qq.com>
2019-07-25 16:27:13 +0800

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -20,29 +20,30 @@ if [ "$DOMAIN" = '' ]; then
     exit 1
 fi
 
-yum install -y unzip zip &&
-mkdir -p /data/docker/nginx/ssl/ &&
-cd /data/docker/nginx/ssl/ &&
+ddd=`yum install -y unzip zip`
+echo $ddd
+mkdir -p /data/docker/nginx/ssl/ 
+cd /data/docker/nginx/ssl/ 
 if [ ! -d /data/docker/nginx/ssl/certbot-master ]; then
-     wget https://github.com/certbot/certbot/archive/master.zip &&
+     wget https://github.com/certbot/certbot/archive/master.zip 
      unzip master.zip  
 fi
 
-cd certbot-master/ &&
-./certbot-auto certonly --standalone --email 137688788@qq.com  -d $DOMAIN --non-interactive --agree-tos &&
+cd certbot-master/ 
+./certbot-auto certonly --standalone --email 137688788@qq.com  -d $DOMAIN --non-interactive --agree-tos 
 
-mkdir -p /data/docker/nginx/ssl/$DOMAIN &&
-cp /etc/letsencrypt/archive/$DOMAIN/fullchain1.pem /data/docker/nginx/ssl/$DOMAIN &&
-cp /etc/letsencrypt/archive/$DOMAIN/privkey1.pem /data/docker/nginx/ssl/$DOMAIN &&
-cd /data/docker/nginx/ssl/$DOMAIN &&
-mv privkey1.pem privkey.pem &&
-mv fullchain1.pem fullchain.pem &&
-chmod 777 privkey.pem &&
-chmod 777 fullchain.pem &&
+mkdir -p /data/docker/nginx/ssl/$DOMAIN 
+cp /etc/letsencrypt/archive/$DOMAIN/fullchain1.pem /data/docker/nginx/ssl/$DOMAIN 
+cp /etc/letsencrypt/archive/$DOMAIN/privkey1.pem /data/docker/nginx/ssl/$DOMAIN 
+cd /data/docker/nginx/ssl/$DOMAIN 
+mv privkey1.pem privkey.pem 
+mv fullchain1.pem fullchain.pem 
+chmod 777 privkey.pem 
+chmod 777 fullchain.pem 
 
 
-ip=$(curl http://ipinfo.io/ip) &&
-log=`tail -n 10 /var/log/letsencrypt/letsencrypt.log | grep 'Your certificate and chain have been saved'` &&
+ip=$(curl http://ipinfo.io/ip) 
+log=`tail -n 10 /var/log/letsencrypt/letsencrypt.log | grep 'Your certificate and chain have been saved'` 
 if [ "$log" == "" ] ; then
 	curl http://118.31.108.209:8999/server/initSsl?ip=$ip&status=2&domain=$DOMAIN
 	echo "init error"
