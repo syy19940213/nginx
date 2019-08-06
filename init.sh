47f1226fc686d63e55da9805406c4f19855e1ab7
syy19940213 <137688788@qq.com>
2019-08-06 11:48:35 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,19 +18,18 @@ sleep 5
 nohup service docker restart >/dev/null 2>&1 &
 
 sleep 10
-echo $ddd
-docker pull nginx 
 docker network create --subnet=$networkip/24 mynetwork 
 
-mkdir -p /data/docker/nginx/logs 
-mkdir -p /data/docker/nginx/ssl 
-mkdir -p /data/docker/nginx/conf/conf.d 
-cd /data/docker/nginx/conf 
-curl -O "https://raw.githubusercontent.com/syy19940213/nginx/master/nginx.conf"
+mkdir -p /data/docker
+cd /data/docker
+curl -O https://raw.githubusercontent.com/syy19940213/nginx/master/Dockerfile
+docker build -t nginx:v1 .
 
+curl -O https://raw.githubusercontent.com/syy19940213/nginx/master/nginx.tar.gz
+tar -xvf nginx.tar.gz 
 
 cd /data/docker/nginx 
-docker run -d -p 80:80 -p 443:443 -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/var/log/nginx -v $PWD/conf/conf.d:/etc/nginx/conf.d -v $PWD/ssl:/ssl  -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone:ro  -v $PWD/www:/www  --net mynetwork --ip $ip   --name mynginx  nginx:latest 
+docker run -d -p 80:80 -p 443:443 -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/var/log/nginx -v $PWD/conf/conf.d:/etc/nginx/conf.d -v $PWD/ssl:/ssl  -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone:ro  -v $PWD/www:/www  --net mynetwork --ip $ip   --name mynginx  nginx::v1 
 docker stop mynginx 
 
 ip=$(curl http://ipinfo.io/ip) 
